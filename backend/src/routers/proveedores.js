2fabd4092fff026226f17e3a40441e022b25292a
Agustin Caviglia <78382@sistemas.frc.utn.edu.ar>
2023-06-23 23:33:43 -0300

    ABMC para proveedores

--- backend/src/routers/proveedores.js
+++ backend/src/routers/proveedores.js
@@ -1,5 +1,5 @@
 const Router = require('express')
-const Proveedores = require("../models/proveedores")
+const Proveedores = require("../models/proveedores");
 
 const routerProveedores = Router();
 
@@ -7,11 +7,40 @@ routerProveedores.post('/', async (req,res)=>{
     try {
         const data = req.body;
         const proveedor = await Proveedores.create(data);
-        res.status(201).json(proveedor);
-        
+        res.status(201).json(proveedor)
       } catch (error) {
         res.status(500).json({ error: 'Error al crear el proveedor' });
       }
+});
+
+routerProveedores.delete('/:nombre', async (req,res)=>{
+    try{
+      const nombre = req.params.nombre
+      await Proveedores.destroy({ where: {nombre}})
+      res.status(200)
+    } catch (e) {
+      res.status(500).json({error: 'Error al eliminar el proveedor'})
+    }
+});
+
+routerProveedores.put('/:id/:nombre', async (req,res)=>{
+  try{
+    const {id, nombre} = req.params
+    const data = req.body
+    const proveedores = await Proveedores.update(data, {where: {id, nombre}})
+    res.status(200).json(proveedores)
+  } catch (e) {
+    res.status(500).json({error: 'Error al actualizar el proveedor'})
+  }
+});
+
+routerProveedores.get('/', async (req,res)=>{
+  try{
+    const proveedores = await Proveedores.findAll({})
+    res.status(200).json(proveedores)
+  } catch (error){
+    res.status(500).json({ error: 'Error al consultar los proveedores' })
+  }
 });
 
 module.exports = routerProveedores
