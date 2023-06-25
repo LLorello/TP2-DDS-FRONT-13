85e862dea496d89780a3301ca45dd3e4d3ce6142
DanielSan <57743@sistemas.frc.utn.edu.ar>
2023-06-25 04:00:11 -0300

    add class container

--- frontend/src/components/productos/FiltroProductos.jsx
+++ frontend/src/components/productos/FiltroProductos.jsx
@@ -5,41 +5,43 @@ function FiltroProductos({ onBuscar, onRegistrar }) {
   const { register, handleSubmit } = useForm();
 
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="card">
-          <div className="card-body">
-            <form onSubmit={handleSubmit(onBuscar)}>
-              <h5 className="card-title">Filtros</h5>
-              <div className="row">
-                <div className="col-auto">Titulo</div>
-                <div className="col-auto">
-                  <input
-                    type="text"
-                    placeholder="Nombre"
-                    {...register("nombre")}
-                  />
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit(onBuscar)}>
+                <h5 className="card-title">Filtros</h5>
+                <div className="row">
+                  <div className="col-auto">Titulo</div>
+                  <div className="col-auto">
+                    <input
+                      type="text"
+                      placeholder="Nombre"
+                      {...register("nombre")}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="row pt-3">
-                <div className="col-3">
-                  <button type="submit" className="btn btn-success w-100">
-                    Consultar
-                  </button>
+                <div className="row pt-3">
+                  <div className="col-3">
+                    <button type="submit" className="btn btn-success w-100">
+                      Consultar
+                    </button>
+                  </div>
+                  <div className="col-3">
+                    <button
+                      onClick={() => {
+                        onRegistrar();
+                      }}
+                      type="button"
+                      className="btn btn-warning w-100"
+                    >
+                      Nuevo
+                    </button>
+                  </div>
                 </div>
-                <div className="col-3">
-                  <button
-                    onClick={() => {
-                      onRegistrar();
-                    }}
-                    type="button"
-                    className="btn btn-warning w-100"
-                  >
-                    Nuevo
-                  </button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
